712450bb706b382cc24f244ff461b6c51360131d
yg409653786 <409653786@qq.com>
2017-01-16 13:35:15 +0800

    new

---
+++ src/com/sunyuki/oa/domain/User.java
@@ -0,0 +1,33 @@
+package com.sunyuki.oa.domain;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+}
---
+++ test/com/sunyuki/oa/mapper/UserMapperTest.java
@@ -0,0 +1,69 @@
+package com.sunyuki.oa.mapper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sunyuki.oa.domain.User;
+
+public class UserMapperTest {
+	
+	private ApplicationContext ac;
+
+	@Before
+	public void setUp() {
+		ac=new ClassPathXmlApplicationContext("classpath:spring.xml");
+	}
+	
+	@Test
+	public void testCountByExample() {
+		UserMapper userMapper=(UserMapper) ac.getBean("userMapper");
+		User record=new User();
+		record.setUsername("测试1");
+		record.setEmail("409653786@qq.com");
+		userMapper.insert(record);
+		System.out.println(userMapper.toString());
+	}
+
+	@Test
+	public void testDeleteByExample() {
+	}
+
+	@Test
+	public void testDeleteByPrimaryKey() {
+	}
+
+	@Test
+	public void testInsert() {
+	}
+
+	@Test
+	public void testInsertSelective() {
+	}
+
+	@Test
+	public void testSelectByExample() {
+	}
+
+	@Test
+	public void testSelectByPrimaryKey() {
+	}
+
+	@Test
+	public void testUpdateByExampleSelective() {
+	}
+
+	@Test
+	public void testUpdateByExample() {
+	}
+
+	@Test
+	public void testUpdateByPrimaryKeySelective() {
+	}
+
+	@Test
+	public void testUpdateByPrimaryKey() {
+	}
+
+}
