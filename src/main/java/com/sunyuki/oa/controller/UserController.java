ded3940cb0c3d65b32261653d2265e252dd0f375
yg409653786 <409653786@qq.com>
2017-01-17 19:14:07 +0800

    new

--- src/main/java/com/sunyuki/oa/controller/UserController.java
+++ src/main/java/com/sunyuki/oa/controller/UserController.java
@@ -6,10 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,7 +29,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="lsit",method={RequestMethod.GET,RequestMethod.POST})
-	public void handleRequest(HttpServletRequest arg0, HttpServletResponse response) throws ServletException, IOException {
+	public void list(HttpServletRequest arg0, HttpServletResponse response,Integer id) throws ServletException, IOException {
 		response.getWriter().print("1234");
 	}
 	
