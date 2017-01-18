6d5c99014622ef8cac53d4ef05ce524db89840e9
yg409653786 <409653786@qq.com>
2017-01-18 10:42:42 +0800

    new

--- src/main/java/com/sunyuki/oa/domain/User.java
+++ src/main/java/com/sunyuki/oa/domain/User.java
@@ -1,8 +1,13 @@
 package com.sunyuki.oa.domain;
 
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+
 public class User {
     private Integer id;
 
+    @Size(min=1,max=30,message="{user.name.length.error}")
+    @Null(message="{user.name.isNotNull}")
     private String username;
 
     private String email;
