0491834f06f9b43193e2751b056ec82c8a16160f
yg409653786 <409653786@qq.com>
2017-01-18 11:01:08 +0800

    new

--- src/main/java/com/sunyuki/oa/controller/UserController.java
+++ src/main/java/com/sunyuki/oa/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.sunyuki.oa.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.sunyuki.oa.domain.UserCustom;
 
 @Controller
 @RequestMapping(value="item")
@@ -25,9 +32,21 @@ public class UserController {
 		return 0;
 	}
 	
+	//@Validated UserCustom userCustom,BindingResult bindingResult
+	//这两个必须成对出现,顺序不可更改
 	@RequestMapping(value="lsit",method={RequestMethod.GET,RequestMethod.POST})
-	public void list(HttpServletRequest arg0, HttpServletResponse response,Integer id) throws ServletException, IOException {
+	public String list(Model model,HttpServletRequest arg0, HttpServletResponse response,Integer id
+			,@Validated UserCustom userCustom,BindingResult bindingResult) throws ServletException, IOException {
+		if(bindingResult.hasErrors()){
+			List<ObjectError> errors = bindingResult.getAllErrors();
+			for(ObjectError error:errors){
+				error.getDefaultMessage();
+			}
+			model.addAttribute("allError", errors);
+			return "";
+		}
 		response.getWriter().print("1234");
+		return "";
 	}
 	
 }
---
+++ src/main/java/com/sunyuki/oa/domain/UserCustom.java
@@ -0,0 +1,5 @@
+package com.sunyuki.oa.domain;
+
+public class UserCustom extends User{
+
+}
