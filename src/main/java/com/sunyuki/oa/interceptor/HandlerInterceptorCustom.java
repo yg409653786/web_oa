9830858a5a98a39dd95ab15eb4c61ec2598c26c6
yanggang <yanggang@sunyuki.com>
2017-01-18 23:17:37 +0800

    new

---
+++ src/main/java/com/sunyuki/oa/interceptor/HandlerInterceptorCustom.java
@@ -0,0 +1,35 @@
+package com.sunyuki.oa.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HandlerInterceptorCustom implements HandlerInterceptor {
+
+	//身份认证
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		// TODO Auto-generated method stub
+		return false;//true 放行,false 拦截
+	}
+
+	//将公用的模型数据(菜单导航),视图传到 modelAndView
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	//统一的异常处理,日志处理
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
