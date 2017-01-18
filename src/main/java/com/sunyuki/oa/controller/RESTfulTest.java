2742412d66e7568ab60e1f48130daf5d82c89874
yanggang <yanggang@sunyuki.com>
2017-01-18 22:45:21 +0800

    new

---
+++ src/main/java/com/sunyuki/oa/controller/RESTfulTest.java
@@ -0,0 +1,28 @@
+package com.sunyuki.oa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sunyuki.oa.domain.UserCustom;
+
+@Controller
+@RequestMapping(value = "/json")
+public class RESTfulTest {
+
+	// json请求 json返回
+	@RequestMapping(value = "/jsonRequest")
+	public @ResponseBody UserCustom jsonRequest(@RequestBody UserCustom userCustom) {
+
+		return userCustom;
+	}
+
+	// json请求 json返回
+	@RequestMapping(value = "/keyvalueRequest")
+	public @ResponseBody UserCustom keyvalueRequest(UserCustom userCustom) {
+
+		return userCustom;
+	}
+
+}
